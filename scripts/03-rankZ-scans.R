94858c949bd228e8ff8fb77e7a7be57a218ae04f
Sue McClatchy <smcclatchy@users.noreply.github.com>
2022-02-28 16:26:29 -0500

    Add files via upload

--- scripts/03-rankZ-scans.R
+++ scripts/03-rankZ-scans.R
@@ -7,16 +7,22 @@ library(qtl2)
 
 # run a genome scan on the rankZ transformed phenotypes
 zScan <- scan1(genoprobs = probs, 
-             pheno = control$pheno[, c("zHeart_GSH", "zHeart_GSSG", "zHeart_TotalGSH")])
+             pheno = control$pheno[, c("zHeart_GSH", 
+                                       "zHeart_GSSG", 
+                                       "zHeart_TotalGSH", 
+                                       "zGSHGSSG_Ratio")])
 
 # look at the first several rows of the scan
 head(zScan)
 
 # LOD score significance thresholds
 perms <- scan1perm(genoprobs = probs, 
-                   pheno = control$pheno[, c("zHeart_GSH", "zHeart_GSSG", "zHeart_TotalGSH")], 
+                   pheno = control$pheno[, c("zHeart_GSH", 
+                                             "zHeart_GSSG", 
+                                             "zHeart_TotalGSH", 
+                                             "zGSHGSSG_Ratio")], 
                    n_perm = 10) # using only 10 permutations for expediency;
-# replace with 1000 before going to press
+                                # replace with 1000 before going to press
 summary(perms)
 thresholds <- summary(perms)
 
@@ -29,13 +35,64 @@ find_peaks(scan1_output = zScan,
 # heart GSH has 3 peaks above the threshold (chr 14, 16 & 19)
 # heart GSSG has 1 peak above the threshold on chr 10
 
+pdf(file = "../results/rankZscans.pdf")
 # plot scans for each phenotype
 plot_scan1(zScan, map = control$gmap, 
            lodcolumn = "zHeart_GSH", main = "RankZ transformed GSH genome scan")
+abline(h = thresholds[1], col = "red", lwd = 2)
+
 plot_scan1(zScan, map = control$gmap, 
            lodcolumn = "zHeart_GSSG", main = "RankZ transformed GSSG genome scan")
+abline(h = thresholds[2], col = "red", lwd = 2)
+
 plot_scan1(zScan, map = control$gmap, 
            lodcolumn = "zHeart_TotalGSH", main = "RankZ transformed total GSH genome scan")
+abline(h = thresholds[3], col = "red", lwd = 2)
 
+plot_scan1(zScan, map = control$gmap, 
+           lodcolumn = "zGSHGSSG_Ratio", main = "RankZ transformed GSH/GSSG Ratio")
+abline(h = thresholds[3], col = "red", lwd = 2)
 
+dev.off()
 
+# for comparison repeat a genome scan on the untransformed phenotypes
+rawScan <- scan1(genoprobs = probs,
+                 pheno = control$pheno[, c("Heart_GSH",
+                                           "Heart_GSSG",
+                                           "Heart_TotalGSH",
+                                           "Heart_GSHGSSGRatio")])
+
+# LOD score significance thresholds
+rawperms <- scan1perm(genoprobs = probs,
+                      pheno = control$pheno[, c("Heart_GSH",
+                                                "Heart_GSSG",
+                                                "Heart_TotalGSH",
+                                                "Heart_GSHGSSGRatio")],
+                      n_perm = 10) # using only 10 permutations for expediency;
+rawthresholds <- summary(rawperms)
+rawthresholds
+find_peaks(scan1_output = rawScan,
+           map = control$gmap,
+           threshold = rawthresholds,
+           prob = 0.95,
+           expand2markers = FALSE)
+
+plot_scan1(rawScan, map = control$gmap, 
+           lodcolumn = "Heart_GSHGSSGRatio", 
+           main = "Untransformed GSH/GSSG Ratio")
+abline(h = rawthresholds[4], col = "red", lwd = 2)
+
+plot_scan1(rawScan, map = control$gmap, 
+           lodcolumn = "Heart_GSH", 
+           main = "Untransformed Heart GSH genome scan")
+abline(h = rawthresholds[1], col = "red", lwd = 2)
+
+plot_scan1(rawScan, map = control$gmap, 
+           lodcolumn = "Heart_GSSG", 
+           main = "Untransformed Heart GSSG genome scan")
+abline(h = rawthresholds[2], col = "red", lwd = 2)
+
+plot_scan1(rawScan, map = control$gmap, 
+           lodcolumn = "Heart_TotalGSH", 
+           main = "Untransformed Total Heart GSH genome scan")
+abline(h = rawthresholds[3], col = "red", lwd = 2)
