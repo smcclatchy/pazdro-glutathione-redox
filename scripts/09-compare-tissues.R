afdf2124c7029473fdfe9d9c9e49709f6f080b68
Sue McClatchy <smcclatchy@users.noreply.github.com>
2023-02-14 17:39:44 -0500

    Add files via upload

--- scripts/09-compare-tissues.R
+++ scripts/09-compare-tissues.R
@@ -27,8 +27,8 @@ panel.hist <- function(x, ...)
 
 # load heart, liver and kidney phenotype data
 load("../data/Heart-GSSG-enviornment.RData")
-liver_pheno <- read_csv("R01_GSH_DO_pheno.csv")
-kidney_pheno <- read_csv(file = "kidney_pheno_covar.csv")
+liver_pheno <- read_csv("../../beccas_qtl2/data/R01_GSH_DO_pheno.csv")
+kidney_pheno <- read_csv(file = "../data/kidney_pheno_covar.csv")
 
 # change variables from character to numeric
 liver_pheno <- liver_pheno %>% mutate_if(is.character, as.numeric)
@@ -38,19 +38,62 @@ kidney_pheno <- kidney_pheno %>% mutate_if(is.character, as.numeric)
 liver_pheno <- liver_pheno %>% mutate("id" = rownames(liver_pheno))
 kidney_pheno <- kidney_pheno %>% mutate("id" = rownames(kidney_pheno))
 
+# calculate redox potential for each tissue
+pheno <- pheno %>% mutate("heart_redox_potential" = (-264 + 31 * log10(Heart_GSSG/Heart_GSH^2)))
+liver_pheno <- liver_pheno %>% mutate("liver_redox_potential" = (-264 + 31 * log10(Liver_Unadj_GSSG/Liver_GSH^2)))
+kidney_pheno <- kidney_pheno %>% mutate("kidney_redox_potential" = (-264 + 31 * log10(Kidney_GSSG/Kidney_GSH^2)))
+
+
 # combine all into a single data frame
+pheno$id <- str_remove(pheno$id, "DO-")
 kidneyLiver <- kidney_pheno %>% left_join(liver_pheno, by = "id")
 kidneyLiverHeart <- kidneyLiver %>% left_join(pheno, by = "id")
 
+
 # scatterplot matrix of GSH, GSSG and total GSH 
-pairs(kidneyLiverHeart[, c(1:3, 10:12, 30:32)], upper.panel = panel.cor, diag.panel = panel.hist)
+pairs(kidneyLiverHeart[, c(2,3,11,28,32,33)], upper.panel = panel.cor, diag.panel = panel.hist)
 
 # explore tissue by tissue
 # kidney and liver
-pairs(kidneyLiverHeart[, c(1:3, 10:12)], upper.panel = panel.cor, diag.panel = panel.hist)
+pairs(kidneyLiverHeart[, c(2:3, 6, 11, 28, 31)], upper.panel = panel.cor, diag.panel = panel.hist)
 
 # kidney and heart
-pairs(kidneyLiverHeart[, c(1:3, 30:32)], upper.panel = panel.cor, diag.panel = panel.hist)
+pairs(kidneyLiverHeart[, c(2:3, 6, 10, 32, 33)], upper.panel = panel.cor, diag.panel = panel.hist)
 
 # liver and heart
 pairs(kidneyLiverHeart[, c(10:12, 30:32)], upper.panel = panel.cor, diag.panel = panel.hist)
+
+# explore linear models of GSH in kidney and heart
+# rankZ transform both
+kidneyLiverHeart <- kidneyLiverHeart %>% 
+  mutate("zkidney_GSH" = rankZ(Kidney_GSH), "zHeart_GSH" = rankZ(Heart_GSH))
+
+summary(lm(kidneyLiverHeart$Heart_GSH ~ kidneyLiverHeart$Kidney_GSH))
+gshModel <- lm(kidneyLiverHeart$Heart_GSH ~ kidneyLiverHeart$Kidney_GSH)
+hist(gshModel$residuals) # residuals not normally distributed
+
+# with rankZ transformed data
+summary(lm(kidneyLiverHeart$zHeart_GSH ~ kidneyLiverHeart$zkidney_GSH))
+
+# now with no intercept
+summary(lm(kidneyLiverHeart$Heart_GSH ~ kidneyLiverHeart$Kidney_GSH - 1))
+summary(lm(kidneyLiverHeart$zHeart_GSH ~ kidneyLiverHeart$zkidney_GSH - 1))
+
+# scatterplot and overlay linear model
+kidneyLiverHeart %>% ggplot(aes(Kidney_GSH, Heart_GSH)) + 
+  geom_point() + geom_smooth(method = "lm")
+# now with no intercept
+kidneyLiverHeart %>% ggplot(aes(Kidney_GSH, Heart_GSH)) + 
+  geom_point() + geom_smooth(method = "lm", formula = 'y ~ x - 1')
+
+# rankZ transformed
+kidneyLiverHeart %>% ggplot(aes(zkidney_GSH, zHeart_GSH)) + 
+  geom_point() + geom_smooth(method = "lm")
+kidneyLiverHeart %>% ggplot(aes(zkidney_GSH, zHeart_GSH)) + 
+  geom_point() + geom_smooth(method = "lm", formula = 'y ~ x - 1')
+
+
+
+
+
+
